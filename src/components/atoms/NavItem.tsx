928f74e623d7871ab209cdbd50fb6ac817dd4654
frrsfuzn <mochfarrasfauzan@gmail.com>
2022-12-06 06:00:06 +0700

    add animation to navitem

--- src/components/atoms/NavItem.tsx
+++ src/components/atoms/NavItem.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import cx from "classnames";
 
 interface NavItemProps {
@@ -7,13 +7,31 @@ interface NavItemProps {
 }
 
 function NavItem({ title, active }: NavItemProps) {
+  let [hover, setHover] = useState(false);
   const containerStyle = cx({
-    "bg-white/100 p-1 mr-5 rounded-sm": true,
+    "relative bg-white/100 mr-2 rounded-sm": true,
     "invert backdrop-invert": active,
+  });
+  let highlight = cx({
+    "transition-all duration-500 bg-white/0 absolute top-0 bottom-0 left-0":
+      true,
+    "backdrop-invert w-full": hover && !active,
+    "w-0": !hover && !active,
   });
   return (
     <div className={containerStyle}>
-      <p className="border-b-2 border-red-400">{title}</p>
+      <div
+        className={highlight}
+        onMouseLeave={() => setHover(false)}
+        onMouseEnter={() => setHover(true)}
+      />
+      <span
+        className="border-b-2 border-red-400"
+        onMouseLeave={() => setHover(false)}
+        onMouseEnter={() => setHover(true)}
+      >
+        {title}
+      </span>
     </div>
   );
 }
