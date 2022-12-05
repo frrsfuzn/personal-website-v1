fb3bba77549e8814a261551cc85a3a90dfbcda61
frrsfuzn <mochfarrasfauzan@gmail.com>
2022-12-06 05:55:21 +0700

    fix highlight animation on menuitem

--- src/components/atoms/MenuItem.tsx
+++ src/components/atoms/MenuItem.tsx
@@ -8,13 +8,14 @@ interface MenuItemProps {
 function MenuItem({title}:MenuItemProps) {
     const [hover, setHover] = useState(false)
     let highlight = cx({
-        'transition-all duration-500 bg-white/0 absolute top-0 bottom-0 left-0 right-0 w-0':true,
+        'transition-all duration-500 bg-white/0 absolute top-0 bottom-0 left-0':true,
         'backdrop-invert w-full': hover,
+        'w-0': !hover
     })
   return (
     <div className='relative inline-block'>
         <div className={highlight} onMouseLeave={() => setHover(false)}  onMouseEnter={() => setHover(true)}/>
-        <span className='border-b-2 border-red-400 z-10' onMouseLeave={() => setHover(false)}  onMouseEnter={() => setHover(true)} >{title}</span>
+        <span className='border-b-2 border-red-400' onMouseLeave={() => setHover(false)}  onMouseEnter={() => setHover(true)} >{title}</span>
     </div>
   )
 }
