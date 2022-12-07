946196bc379391b13ff6afab4ca80fb153c10bb8
frrsfuzn <mochfarrasfauzan@gmail.com>
2022-12-08 05:30:39 +0700

    add experience data

--- pages/experience.tsx
+++ pages/experience.tsx
@@ -4,7 +4,31 @@ import Layout from "../src/components/layout";
 function Experience() {
   return (
     <Layout>
-      <div>Experience</div>
+      <div className="mx-auto max-w-5xl w-full">
+        <h1 className="text-2xl mb-10">Experience</h1>
+        <div>
+          <div className="flex flex-row justify-center items-center">
+            <div className="h-0.5 flex-1 bg-slate-500"/>
+            <p className="mx-5">January 2022-November 2022</p>
+            <div className="h-0.5 flex-1 bg-slate-500"/>
+          </div>
+          <div className="flex flex-row justify-between mb-5">
+            <h1 className="text-2xl">Ruangguru</h1>
+            <h2 className="text-2xl">Frontend Engineer</h2>
+          </div>
+        </div>
+        <div>
+          <div className="flex flex-row justify-center items-center">
+            <div className="h-0.5 flex-1 bg-slate-500"/>
+            <p className="mx-5">September 2021-December 2021</p>
+            <div className="h-0.5 flex-1 bg-slate-500"/>
+          </div>
+          <div className="flex flex-row justify-between">
+            <h1 className="text-2xl">Siklus</h1>
+            <h2 className="text-2xl">Frontend Engineer Intern</h2>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 }
