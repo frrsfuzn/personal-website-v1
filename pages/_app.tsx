953acac060972e8e2d94dc3a0ed197fb90d8de7e
frrsfuzn <mochfarrasfauzan@gmail.com>
2022-12-09 16:57:02 +0700

    add google tag analytic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,32 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import Router from 'next/router';
-import NProgress from 'nprogress'; //nprogress module
-import 'nprogress/nprogress.css'; //styles of nprogress
+import Router from "next/router";
+import NProgress from "nprogress"; //nprogress module
+import "nprogress/nprogress.css"; //styles of nprogress
+import Script from "next/script";
 
-Router.events.on('routeChangeStart', () => NProgress.start()); 
-Router.events.on('routeChangeComplete', () => NProgress.done()); 
-Router.events.on('routeChangeError', () => NProgress.done());
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
-NProgress.configure({ showSpinner: false })
+NProgress.configure({ showSpinner: false });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-HZW9SL0R1K"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-HZW9SL0R1K');
+                `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
